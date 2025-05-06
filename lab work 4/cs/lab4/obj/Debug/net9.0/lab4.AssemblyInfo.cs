9978b50a20c8bbb0a59e71f35f9259fc41657899
fakcing <ar3wehuman@gmail.com>
2025-05-07 01:29:49 +0300

    inself

--- lab work 4/cs/lab4/obj/Debug/net9.0/lab4.AssemblyInfo.cs
+++ lab work 4/cs/lab4/obj/Debug/net9.0/lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0709f6899056f6dd35e252416f1f96b71017320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ee2e6da34f9dd3850b0c5b724880c85b1ba1bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
